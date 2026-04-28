ec5cedc56407b3d6962f7f78112e5b2c3a5460fc
Utkarsh-git-init <utmaurya2312@gmail.com>
2026-04-28 15:55:18 +0530

    added moviePage

--- src/MoviePage/MoviePageUpper.jsx
+++ src/MoviePage/MoviePageUpper.jsx
@@ -1,7 +1,30 @@
+import './moviePageUpper.css'
+
 function MoviePageUpper({movie}){
+    const releasedate=function (){
+        const rd=movie.release_date;
+        return new Date(rd).toLocaleDateString('en-US',{
+            year:'numeric',
+            month:'long',
+            day:'numeric'}).replace(/\//g, '');
+    }
     return (
         <>
-            <p>{movie.title}</p>
+            <div className={"moviePageUpper"}>
+                <img id={"backdrop"} src={movie.backdrop_path}/>
+                <div className={"detailsAndPoster"}>
+                    <img src={movie.poster_path}/>
+                    <div className={"details"}>
+                        <div className={"titleAndReleaseDate"}>
+                            <h2>{movie.title}</h2>
+                            <p>{releasedate()}</p>
+                        </div>
+                        <p id={"tagline"}>{movie.tagline}</p>
+                        <p id={"overview"}>{movie.overview}</p>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
